143472ffc827c5194360fcf235827454f91244ea
Joacim <jwideving@gmail.com>
2023-12-02 10:41:30 +0100

    removed old files

--- day1/solution.ts
+++
@@ -1,9 +0,0 @@
-import parseData from 'utils/data-parser'
-
-const data = await parseData(Bun.file('day1/data.txt'))
-const test = await parseData(Bun.file('day1/test.txt'))
-
-const solution = (data: string[]) => {}
-
-solution(test)
-// solution(data)
