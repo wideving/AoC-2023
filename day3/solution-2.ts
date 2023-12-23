8ca2d279bf1a6f1a45296b0b64be43bda30029bc
Joacim <jwideving@gmail.com>
2023-12-23 14:40:18 +0100

    day3 b

--- day3/solution-2.ts
+++ day3/solution-2.ts
@@ -25,6 +25,7 @@ const solution = (data: string[]) => {
     matches.forEach((match) => {
       const regex = new RegExp(rawFindRegex.replace('{replace}', match), 'gm')
       const execMatch = regex.exec(line)
+
       let startIndex = execMatch!.index
 
       const previous = acc.find(
@@ -46,72 +47,119 @@ const solution = (data: string[]) => {
     return acc
   }, new Array<MaybePartNumber>())
 
-  const partSums = maybePartNumbers.reduce((acc, { row, start, end, digits }) => {
+  const potentialGear = maybePartNumbers.reduce((acc, { row, start, end, digits }) => {
     // check left
-    if (data[row][start - 1]?.match(symbolRegex)) {
-      return acc + digits
+    const left = data[row][start - 1]?.match(symbolRegex)
+
+    if (left) {
+      savePotentialGear(left.toString(), start - 1, row, digits, acc)
+      return acc
     }
 
     // check right
-    if (data[row][end]?.match(symbolRegex)) {
-      return acc + digits
+    const right = data[row][end]?.match(symbolRegex)
+
+    if (right) {
+      savePotentialGear(right.toString(), end, row, digits, acc)
+      return acc
     }
 
     // check top
     if (row > 0) {
       const isAtStart = start === 0
+
       if (isAtStart) {
         const substring = data[row - 1].substring(start, end + 1)
+
         if (substring.match(symbolRegex)) {
-          return acc + digits
+          savePotentialGear(substring.toString(), start, row - 1, digits, acc)
+          return acc
         }
       }
 
       const isAtEnd = end === maxLength
+
       if (isAtEnd) {
         const substring = data[row - 1].substring(start - 1, end)
+
         if (substring.match(symbolRegex)) {
-          return acc + digits
+          savePotentialGear(substring.toString(), start - 1, row - 1, digits, acc)
+          return acc
         }
       }
+
       const substring = data[row - 1].substring(start - 1, end + 1)
+
       if (substring.match(symbolRegex)) {
-        return acc + digits
+        savePotentialGear(substring.toString(), start - 1, row - 1, digits, acc)
+        return acc
       }
     }
 
     // check bottom
     if (row < data.length - 1) {
       const isAtStart = start === 0
+
       if (isAtStart) {
         const substring = data[row + 1].substring(start, end + 1)
+
         if (substring.match(symbolRegex)) {
-          return acc + digits
+          savePotentialGear(substring.toString(), start, row + 1, digits, acc)
+          return acc
         }
       }
 
       const isAtEnd = end === maxLength
+
       if (isAtEnd) {
         const substring = data[row + 1].substring(start - 1, end)
+
         if (substring.match(symbolRegex)) {
-          return acc + digits
+          savePotentialGear(substring.toString(), start - 1, row + 1, digits, acc)
+          return acc
         }
       }
 
       const substring = data[row + 1].substring(start - 1, end + 1)
+
       if (substring.match(symbolRegex)) {
-        return acc + digits
+        savePotentialGear(substring.toString(), start - 1, row + 1, digits, acc)
+        return acc
       }
     }
 
     return acc
-  }, 0)
+  }, new Map<string, Array<number>>())
 
-  return partSums
+  return Array.from(potentialGear.entries()).reduce((acc, [key, value]) => {
+    if (value.length === 2) {
+      return acc + value[0] * value[1]
+    } else {
+      return acc
+    }
+  }, 0)
 }
 
-const result = solution(test)
-console.log('result', result)
+const savePotentialGear = (
+  symbol: string,
+  start: number,
+  row: number,
+  digits: number,
+  potentialGear: Map<string, Array<number>>
+) => {
+  const indexOfSymbol = symbol.indexOf('*')
+  if (indexOfSymbol !== -1) {
+    const x = start + indexOfSymbol
+    const y = row
 
-// const result = solution(data)
-// console.log(result)
+    const coordinates = `${x},${y}`
+    const entry = potentialGear.get(coordinates) ?? []
+    entry.push(digits)
+    potentialGear.set(coordinates, entry)
+  }
+}
+// const result = solution(test)
+// console.log('result', result)
+
+const result = solution(data)
+console.log(result)
